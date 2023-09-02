47fd704a324b640b66341666ef0a00ecc3de1426
Sitt Hmue Eain <sitthmueeain@gmail.com>
2023-09-03 00:43:30 +0630

    Conditional_rendering_with_tenaryOperator

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,7 +1,18 @@
 export default function Profile({isAdmin} : any) {
-  if (isAdmin) {
-    return (<div>Admin</div>);
-  } else {
-    return (<div>User</div>);
-  }
+    
+    //method 1
+//   if (isAdmin) {
+//     return (<div>Admin</div>);
+//   } else {
+//     return (<div>User</div>);
+//   }
+
+    //method 2
+    return (
+        <div>
+            {
+                isAdmin? <div>Admin</div> : <div>User</div>
+            }
+        </div>
+    )
 }
